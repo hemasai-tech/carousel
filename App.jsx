cc172f2940297ebbd025871733bac124e1efcbd9
hemasai-tech <hema.saik@yahoo.com>
2023-09-28 19:58:20 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Animation from './src/components/Animation';
+
+const App = () => {
+  return <Animation />;
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 50,
+    paddingHorizontal: 2,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
